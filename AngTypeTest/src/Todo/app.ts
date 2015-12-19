59c3dc527e919147fcb5ffcdabef35d21b8100ed
michael berezin <mbearz@gmail.com>
2015-12-20 00:43:29 +0200

    tag filter

--- AngTypeTest/src/Todo/app.ts
+++ AngTypeTest/src/Todo/app.ts
@@ -14,7 +14,8 @@ import {Http, HTTP_BINDINGS, Headers} from "angular2/http";
 
 })
 
-class AppComponent {
+class AppComponent
+{
     public title = "Michaels TODO app";
     public navBarLInkText = "a random link";
     public selectedItem: ListItem;
@@ -28,26 +29,21 @@ class AppComponent {
     public headers: Headers;
     public items = new Array<ListItem>();
     public types = new Array<ItemType>();
+    public filerTypes = new Array<ItemType>();
     public selectedType: ItemType;
-    public selectedTypeId: number;
+
 
     public searchTerm: string;
 
 
-    constructor(http: Http) {
+    constructor(http: Http)
+    {
         this.config = new Config();
         this.http = http;
         this.headers = new Headers();
         this.headers.append('Content-Type', 'application/json');
         this.addingNew = false;
         this.searchTerm = "";
-        //this.http.get(this.config.apiBaseUrl + "ItemTypes")
-        //    .map(res=> res.json())
-        //    .subscribe(
-        //    data => this.loadTypes(data),
-        //    err=> console.log(err),
-        //    () => { }
-        //    );
 
         this.http.get(this.config.apiBaseUrl + "Todo")
             .map(res=> res.json())
@@ -58,16 +54,19 @@ class AppComponent {
             );
     }
 
-    loadTypes(itemList: any, list: any) {
-        for (var i = 0; i < list.length; i++) {
+    loadTypes(itemList: any, list: any)
+    {
+        for (var i = 0; i < list.length; i++)
+        {
             this.types.push(new ItemType(list[i].Id, list[i].Name));
         }
 
         this.selectedType = this.types[0];
-        this.selectedTypeId = this.selectedType.id;
 
-        for (var i = 0; i < itemList.length; i++) {
-            var type = this.getType(itemList[i].Type); 
+
+        for (var i = 0; i < itemList.length; i++)
+        {
+            var type = this.getType(itemList[i].Type);
 
             this.items.push(new ListItem(itemList[i].Id, itemList[i].Name, itemList[i].IsDone, type));
         }
@@ -77,7 +76,8 @@ class AppComponent {
         this.remainingItemsCount = this.items.length - this.doneItems.length;
     }
 
-    loadItems(itemList: any) {
+    loadItems(itemList: any)
+    {
 
         this.http.get(this.config.apiBaseUrl + "ItemTypes")
             .map(res=> res.json())
@@ -88,27 +88,59 @@ class AppComponent {
             );
     }
 
-    search(term: string) {
+    search(term: string)
+    {
 
         term = term.toLowerCase();
-        if (!term) {
+        if (!term)
+        {
             this.doneItems = this.items.where(function () { return this.isDone; });
             this.unDoneItems = this.items.where(function () { return !this.isDone; });
-        } 
-        else {
-            this.doneItems = this.items.where(function () { return this.isDone && this.name.toLowerCase().indexOf(term)>-1; });
+        }
+        else
+        {
+            this.doneItems = this.items.where(function () { return this.isDone && this.name.toLowerCase().indexOf(term) > -1; });
             this.unDoneItems = this.items.where(function () { return !this.isDone && this.name.toLowerCase().indexOf(term) > -1; });
         }
-        
+
     }
 
-    getType(id) {
+    toggleTypeFilter(type: ItemType)
+    {
+        var index = this.filerTypes.indexOf(type);
 
-        if (this.types) {
+        if (index == -1)
+        {//add
+            this.filerTypes.push(type);
+        }
+        else
+        {//remove
+            this.filerTypes.slice(index, 1);
+        }
+
+        if (this.filerTypes.length > 0)
+        {
+            var cloneTypes = this.filerTypes;
+            this.doneItems = this.items.where(function () { return this.isDone && cloneTypes.indexOf(this.itemType) > -1; });
+            this.unDoneItems = this.items.where(function () { return !this.isDone && cloneTypes.indexOf(this.itemType) > -1; });
+        }
+        else
+        {
+            this.doneItems = this.items.where(function () { return this.isDone; });
+            this.unDoneItems = this.items.where(function () { return !this.isDone; });
+        }
+    }
+
+    getType(id)
+    {
+
+        if (this.types)
+        {
             return this.types.first(function () { return this.id == id });
         }
     }
-    onChange(item: ListItem) {
+    onChange(item: ListItem)
+    {
         item.isDone = !item.isDone;
         this.doneItems = this.items.where(function () { return this.isDone; });
         this.unDoneItems = this.items.where(function () { return !this.isDone; });
@@ -116,14 +148,17 @@ class AppComponent {
     };
 
 
-    addItem(event: any) {
+    addItem(event: any)
+    {
         this.addingNew = true;
         this.newItem = ListItem.CreateEmptyListItem();
     }
 
-    SaveNewItem() {
+    SaveNewItem()
+    {
 
-        if (this.newItem.itemType.id === 0 || this.newItem.name.trim() === '') {
+        if (this.newItem.itemType.id === 0 || this.newItem.name.trim() === '')
+        {
             alert("You must fill all of the data!");
             return;
         }
@@ -141,29 +176,35 @@ class AppComponent {
             );
     }
 
-    saveCallback(data: any) {
+    saveCallback(data: any)
+    {
 
         this.unDoneItems.push(new ListItem(data.Id, this.newItem.name, false, this.getType(data.Type)));
     }
 
-    cancel() {
+    cancel()
+    {
         this.addingNew = false;
     }
-    onSelect(item: ListItem) {
+    onSelect(item: ListItem)
+    {
         this.selectedItem = item;
-       // this.selectedTypeId = item.type;
+        // this.selectedTypeId = item.type;
     };
 
-    setClass(item: ListItem) {
+    setClass(item: ListItem)
+    {
         return item.isDone ? "label-success" : "label-info";
     };
 
 
-    getSelectedClass(item: ListItem) {
+    getSelectedClass(item: ListItem)
+    {
         return { "selected": item === this.selectedItem };
     };
 
-    updateName(item: ListItem) {
+    updateName(item: ListItem)
+    {
         var data = JSON.stringify(this.selectedItem);
         this.http.put(this.config.apiBaseUrl + "Todo", data, {
             headers: this.headers
@@ -176,7 +217,8 @@ class AppComponent {
             );
     }
 
-    showAlert(data: any) {
+    showAlert(data: any)
+    {
         alert("ok");
     }
 }
